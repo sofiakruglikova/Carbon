4da365808e60c005eafafeab1ae015d946a5b574
Sofiia Kruglikova <sofiakruglikova@Sofiias-MacBook.local>
2021-03-04 09:34:56 +0000

    Add Data Preparation

---
+++ cfa/cfa/Modules /AddData/AddDataModel.swift
@@ -0,0 +1,12 @@
+//
+//  AddDataModel.swift
+//  cfa
+//
+//  Created by Sofiia Kruglikova on 04.03.2021.
+//
+
+import Foundation
+
+enum AddData {
+    
+}
---
+++ cfa/cfa/Modules /AddData/AddDataViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AddDataViewModel.swift
+//  cfa
+//
+//  Created by Sofiia Kruglikova on 04.03.2021.
+//
+
+import Foundation
+
+class AddDataViewModel {
+    
+}
